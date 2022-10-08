dd0c200f9f132da98cd62e8fee347544ce5683a9
David Adewale <dadewale9@gmail.com>
2022-10-08 17:51:50 +0100

    bot side added

--- src/chat/chat.js
+++ src/chat/chat.js
@@ -26,7 +26,9 @@ const createMessageContainer = () =>{
     container.classList.add("vanilla-chatbot-kit-chat-message-container")
 
     const userMessage = createUserChatMessage("Hello World!")
+    const botMessage = createBotChatMessage("Hello User!")
     container.appendChild(userMessage);
+    container.appendChild(botMessage);
 
     return container;
 }
@@ -88,8 +90,33 @@ const createUserChatMessage = (message) => {
 
 
 
-const createBotChatMessage = () => {
+const createBotChatMessage = (message) => {
+    const container = document.createElement('div')
+    container.classList.add("vanilla-chatbot-kit-chat-bot-message-container")
 
+    const avatarContainer = document.createElement('div')
+    avatarContainer.classList.add("vanilla-chatbot-kit-chat-bot-avatar-container")
+
+    const paragraph = document.createElement('p')
+    paragraph.textContent = "B"
+    paragraph.classList.add("vanilla-chatbot-kit-chat-bot-avatar-letter");
+
+    const messageContainer = document.createElement("div");
+    messageContainer.classList.add("vanilla-chatbot-kit-chat-bot-message")
+    messageContainer.textContent = message;
+
+    const arrow = document.createElement("div");
+    arrow.classList.add("vanilla-chatbot-kit-chat-bot-message-arrow")
+
+    messageContainer.appendChild(arrow)
+    avatarContainer.appendChild(paragraph)
+
+    container.appendChild(avatarContainer)
+    container.appendChild(messageContainer)
+
+    return container;
+
+    
 }
 
 
